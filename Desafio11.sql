d95b44cf17efccd05966b26cc99901de148eb20f
Eliel Raposo da silva <32067603+ElielSilva@users.noreply.github.com>
2023-10-18 23:07:51 -0300

    Create Desafio11.sql
    consulta nome e genero interligado com as tabelas filmes , generos e filmesgenero

---
+++ Desafio11.sql
@@ -0,0 +1,10 @@
+SELECT 
+  [FIL].Nome
+  ,[GEN].Genero
+  FROM [dbo].[FilmesGenero] AS FG
+  INNER JOiN [dbo].[Filmes] AS FIL
+  ON FIL.Id = FG.IdFilme
+  INNER JOiN [dbo].[Generos] AS GEN
+  ON GEN.Id = FG.IdGenero
+  WHERE [GEN].Genero = 'Mistério'
+GO
